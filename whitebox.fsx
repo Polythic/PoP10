e8267e20b6bda5cd32ed9e66b14b1da7ccc73d9b
Simon <simongram@me.com>
2019-01-14 10:40:46 +0100

    Move whitebox to own file

---
+++ whitebox.fsx
@@ -0,0 +1,71 @@
+printfn "Whitebox test af getNeighbourfields" 
+printfn " Branch: 1a - %b" (getNeighbourfields (10,7) boardWidth 7 = true)
+printfn " Branch: 1a - %b" (getNeighbourfields (10,7) boardWidth 7 = true)
+printfn " Branch: 1a - %b" (getNeighbourfields (10,7) boardWidth 7 = true)
+printfn " Branch: 1a - %b" (getNeighbourfields (10,7) boardWidth 7 = true)
+
+printfn "Whitebox test af getSymbolFromPosition"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af getNeighbourSymbols"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af availableSymbolField"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af Animal"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af Moose"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af Wolf"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af board"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af environment"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af Draw"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af AnyEmptyFields"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+
+printfn "Whitebox test af runSimulationTicks"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
