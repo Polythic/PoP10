66b9fa61ac79cbca320241e32cc4cde57d0a7672
Simon <simongram@me.com>
2019-01-14 11:16:26 +0100

    Update whitebox.fsx

--- whitebox.fsx
+++ whitebox.fsx
@@ -30,15 +30,16 @@ printfn " Branch: 1d - %b" (getSymbolFromPosition testPosition4 test2DArray = 'c
 printfn " Branch: 1e - %b" (getSymbolFromPosition testPosition5 test2DArray = 'd')
 
 printfn "Whitebox test af getNeighbourSymbols"
-printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition1 widthOfBoard = )
-printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition2 widthOfBoard = )
-printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition3 widthOfBoard = )
-printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition4 widthOfBoard = )
-printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition5 widthOfBoard = )
-printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition6 widthOfBoard = )
-
+printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition1 test2DArray widthOfBoard = [|((4, 4), ' '); ((5, 4), ' '); ((6, 4), ' '); ((6, 5), ' '); ((6, 6), ' '); ((5, 6), ' '); ((4, 6), ' '); ((5, 4), ' ')|])
+printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition2 test2DArray widthOfBoard = [|((0, 1), ' '); ((1, 1), ' '); ((1, 0), ' ')|])
+printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition3 test2DArray widthOfBoard = [|((0, 8), ' '); ((1, 8), ' '); ((1, 9), ' ')|])
+printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition4 test2DArray widthOfBoard = [|((8, 8), ' '); ((9, 8), ' '); ((8, 9), ' ')|])
+printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition5 test2DArray widthOfBoard = [|((0, 4), ' '); ((1, 4), ' '); ((1, 5), ' '); ((1, 6), ' '); ((0, 6), ' ')|])
+printfn " Branch: 1a - %b" (getNeighbourSymbols testPosition6 test2DArray widthOfBoard = [|((4, 9), ' '); ((4, 8), ' '); ((5, 8), ' '); ((6, 8), ' '); ((6, 9), ' ')|])
+(*
 printfn "Whitebox test af availableSymbolField"
 printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
 printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
 printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
 printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+*)
